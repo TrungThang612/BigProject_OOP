37f312dc7f9e7ccbb9d46dd63340f14f540c1a4b
TrungThang612 <2151050415thang@ou.edu.vn>
2022-12-15 14:44:26 +0700

    finished 1

--- src/main/java/btlon/BTL.java
+++ src/main/java/btlon/BTL.java
@@ -39,6 +39,7 @@ public class BTL {
             System.out.print("Nhập lựa chọn của bạn: ");
             String choice = Tool.sc.nextLine();
             switch (choice) {
+
                 case "1" -> {
                     System.out.print("Nhập số sản phẩm: ");
                     int n = Integer.parseInt(Tool.sc.nextLine());
@@ -156,6 +157,7 @@ public class BTL {
                 }
                 case "7" -> {
                     pm.removeDiscountsByOutDate();
+                    am.removeDiscountsOutDate();
                     System.out.println("Đã xóa khuyến mãi hết hạn của các sản phẩm!");
                 }
                 case "8" -> {
@@ -225,7 +227,7 @@ public class BTL {
         }
     }
 }
-//
+
 //package btlon;
 //
 //class Demo {
@@ -237,14 +239,18 @@ public class BTL {
 //
 //        System.out.println(b.betweenDate());
 //        Product p1 = new Product();
-//        p1.input();
 //        p1.addDiscount(b, a, c);
 //        System.out.println(p1.countDisUpToDate());
 //        System.out.println("==========");
 //        Product p2 = new Product();
-//        p2.input();
-//        System.out.println(p1.countDisUpToDate());
+//
+//        p2.addDiscount(b,a,d,c);
+//        System.out.println(p2.countDisUpToDate());
 //        System.out.println("==========");
+//        if (p1.compareProductByDis(p2)==1){
+//            System.out.println("p1 lon");
+//        }else
+//            System.out.println("p2 lon");
 //
 //    }
 //}
